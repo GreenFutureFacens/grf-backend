f8b008392c04c3185c0377a3016ae7e97b7d067f
Felipe Lima <felipe.pedroso2108@gmail.com>
2024-09-18 21:58:43 -0300

    :memo: init

---
+++ src/main/java/com/queimadas/focos/FocosApplication.java
@@ -0,0 +1,13 @@
+package com.queimadas.focos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FocosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FocosApplication.class, args);
+	}
+
+}
